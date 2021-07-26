b6941a3cf416847a9458956cae80921c0e41bd11
Andej Nesterenko <mentatus.n@gmail.com>
2021-07-26 23:20:44 +0300

    Реализовали Фасад Для Domain Model https://habr.com/ru/post/321050/ https://habr.com/ru/post/322700/
    Интерфейс Reposiry
    его имплементация ReposiryImpl
    
    по любым вопросам телеграмм @nesstar

--- app/src/main/java/calculator/calulation/lesson2/view/MainFragment.kt
+++ app/src/main/java/calculator/calulation/lesson2/view/MainFragment.kt
@@ -12,7 +12,9 @@ import androidx.lifecycle.ViewModelProvider
 import calculator.calulation.lesson2.R
 import calculator.calulation.lesson2.databinding.ActivityMainBinding
 import calculator.calulation.lesson2.databinding.MainFragmentBinding
+import calculator.calulation.lesson2.viewmodel.AppState
 import calculator.calulation.lesson2.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainFragment : Fragment() {
 
@@ -26,7 +28,6 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance()= MainFragment()
     }
-
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
         binding = MainFragmentBinding.inflate(inflater, container, false)
         return binding.root
@@ -35,9 +36,32 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val observer = Observer<Any>{ Toast.makeText(context,"Работает ",Toast.LENGTH_LONG).show()}
-        viewModel.getLiveData().observe(viewLifecycleOwner,observer)
-        viewModel.getDataFromLocalSource()
+        //val observer = Observer<Any>{ Toast.makeText(context,"Работает ",Toast.LENGTH_LONG).show()}
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it) })
+        viewModel.getWaether()
     }
+
+    private fun renderData(appState: AppState) {
+        when(appState){
+            is AppState.Error -> TODO() //show errors
+            is AppState.Success -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainView,"Success",Snackbar.LENGTH_LONG).show()
+                setData(appState)
+            }
+            AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun setData(appState: AppState.Success) {
+        binding.cityCoordinates.text =
+            "${appState.dataWeather.city.lat} ${appState.dataWeather.city.long}"
+        binding.cityName.text = appState.dataWeather.city.city
+        binding.feelsLikeValue.text = appState.dataWeather.temerature.toString()
+        binding.temperatureValue.text = appState.dataWeather.feelsLike.toString()
+    }
+
 
 }
