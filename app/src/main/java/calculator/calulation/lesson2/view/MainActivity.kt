fafbf903462c88b78861ccf866be7554f9131fe2
Andej Nesterenko <mentatus.n@gmail.com>
2021-07-26 22:28:43 +0300

    Реализовали ViewBinding
    Достигли null-safety
    Улушили производительность

--- app/src/main/java/calculator/calulation/lesson2/view/MainActivity.kt
+++ app/src/main/java/calculator/calulation/lesson2/view/MainActivity.kt
@@ -3,11 +3,16 @@ package calculator.calulation.lesson2.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import calculator.calulation.lesson2.R
+import calculator.calulation.lesson2.databinding.ActivityMainBinding
+import calculator.calulation.lesson2.databinding.MainFragmentBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if(savedInstanceState==null){
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container,MainFragment.newInstance()).commit()
         }
--- app/src/main/java/calculator/calulation/lesson2/view/MainFragment.kt
+++ app/src/main/java/calculator/calulation/lesson2/view/MainFragment.kt
@@ -4,23 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import calculator.calulation.lesson2.R
+import calculator.calulation.lesson2.databinding.ActivityMainBinding
+import calculator.calulation.lesson2.databinding.MainFragmentBinding
 import calculator.calulation.lesson2.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
     lateinit var viewModel: MainViewModel
+    //var _binding: MainFragmentBinding? = null
+    lateinit var binding:MainFragmentBinding
+    /*    get() :MainFragmentBinding{
+            return _binding!!
+        }*/
 
     companion object {
         fun newInstance()= MainFragment()
     }
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        return  inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
