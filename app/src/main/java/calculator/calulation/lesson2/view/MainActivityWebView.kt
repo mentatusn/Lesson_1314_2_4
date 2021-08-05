227bc614c30e7e4350843854cda01232f2a93361
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-05 21:08:43 +0300

    Написали свой браузер MainActivityWebView.kt
    С использованием webview

---
+++ app/src/main/java/calculator/calulation/lesson2/view/MainActivityWebView.kt
@@ -0,0 +1,58 @@
+package calculator.calulation.lesson2.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import calculator.calulation.lesson2.R
+import calculator.calulation.lesson2.databinding.ActivityMainBinding
+import calculator.calulation.lesson2.databinding.MainActivityWebviewBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.URL
+import java.util.stream.Collector
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
+
+class MainActivityWebView : AppCompatActivity() {
+
+
+    lateinit var binding: MainActivityWebviewBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityWebviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ok.setOnClickListener(clickListener)
+    }
+
+    var clickListener: View.OnClickListener = object : View.OnClickListener {
+        override fun onClick(v: View) {
+
+            Thread {
+                var httpsURLConnection: HttpsURLConnection? = null
+                try {
+                    val url = URL(binding.url.text.toString())
+                    httpsURLConnection = url.openConnection() as HttpsURLConnection
+                    httpsURLConnection.requestMethod = "GET"
+                    httpsURLConnection.connectTimeout = 5000
+
+                    var reader = BufferedReader(InputStreamReader(httpsURLConnection.inputStream))
+                    val result = reader.lines().collect(Collectors.joining("\n"))
+                    Log.d("mylogs", result)
+                    runOnUiThread{
+                        //binding.webView.loadData(result,"text/html; charset = utf-8","utf-8")
+                        binding.webView.loadDataWithBaseURL(null,result,"text/html; charset = utf-8","utf-8",null)
+                    }
+
+               } catch (e: ExceptionInInitializerError) {
+                    Log.d("mylogs", e.toString())
+                    Log.d("mylogs", e.stackTraceToString())
+                    Log.d("mylogs", e.localizedMessage)
+                }
+            }.start()
+        }
+    }
+
+
+}
