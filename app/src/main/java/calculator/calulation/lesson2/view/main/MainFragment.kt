de8ccb38aed8e6a3065f483291c52aeac34baef3
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-23 21:30:41 +0300

    Урок 10 реализовали геолокацию

--- app/src/main/java/calculator/calulation/lesson2/view/main/MainFragment.kt
+++ app/src/main/java/calculator/calulation/lesson2/view/main/MainFragment.kt
@@ -1,14 +1,26 @@
 package calculator.calulation.lesson2.view.main
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import calculator.calulation.lesson2.R
 import calculator.calulation.lesson2.databinding.FragmentMainBinding
+import calculator.calulation.lesson2.model.City
 import calculator.calulation.lesson2.model.Weather
 import calculator.calulation.lesson2.view.details.DetailsFragment
 import calculator.calulation.lesson2.viewmodel.AppState
@@ -35,16 +47,21 @@ class MainFragment : Fragment() {
             }*/
 
             override fun onItemViewClick(weather: Weather) {
-                activity?.supportFragmentManager?.apply {
-                    beginTransaction()
-                        .add(R.id.container, DetailsFragment.newInstance(Bundle().apply {
-                            putParcelable(DetailsFragment.KEY_WEATHER, weather)
-                        }))
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
-                }
+                openDetailsFragment(weather)
             }
         })
+
+    private fun openDetailsFragment(weather: Weather) {
+        activity?.supportFragmentManager?.apply {
+            beginTransaction()
+                .add(R.id.container, DetailsFragment.newInstance(Bundle().apply {
+                    putParcelable(DetailsFragment.KEY_WEATHER, weather)
+                }))
+                .addToBackStack("")
+                .commit()
+        }
+    }
+
 
     /* Вариант с отложенной инициализацией вместо lazy
     lateinit var viewModel: MainViewModel*/
@@ -77,11 +94,141 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    private fun showRatio() {
+        showDialog(getString(R.string.dialog_rationale_title) ,getString(R.string.dialog_rationale_meaasge))
+    }
+
+
+    private fun showDialog(title:String,message: String) {
+        context?.let {
+            AlertDialog.Builder(it)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Пускай работает корректно") { _, _ ->
+                    requestPermission()
+                }
+                .setNegativeButton("Пускай не работает") { dialog, _ ->
+                    dialog.dismiss()
+                    requireActivity().finish()
+                }
+                .create()
+                .show()
+        }
+    }
+
+    private fun showAddressDialog(message: String, location: Location) {
+        context?.let {
+            AlertDialog.Builder(it)
+                .setTitle(R.string.dialog_address_title)
+                .setMessage(message)
+                .setPositiveButton(R.string.dialog_address_get_weather) { _, _ ->
+                    openDetailsFragment(Weather(City(message,location.latitude,location.longitude)))
+                }
+                .setNegativeButton(R.string.dialog_button_close) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+    }
+
+    private val REQUEST_CODE = 1
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
+
+    private fun checkPermission() {
+        context?.let {
+            if (ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PermissionChecker.PERMISSION_GRANTED
+            ) {
+                getLocation()
+            } else {
+
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    showRatio()
+                } else {
+                    requestPermission()
+                }
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+                    getLocation()
+                } else {
+                    context?.let { showRatio() }
+                }
+            }
+        }
+    }
+
+    private fun getAddress(context: Context,location: Location){
+        val geoCoder = Geocoder(context)
+        val address = geoCoder.getFromLocation(location.latitude,location.longitude,1)
+        showAddressDialog(address[0].getAddressLine(0),location)
+    }
+
+    private val REFRESH_PERIOD = 6000L
+    private val MINIMAL_DISTANCE = 100f
+
+    private val locationListener = object : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            getAddress(requireActivity(),location)
+        }
+
+    }
+
+    private fun getLocation(){
+        activity?.let { context->
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PermissionChecker.PERMISSION_GRANTED
+            ){
+                val locationManager  = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                    val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+                    provider?.let{
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            REFRESH_PERIOD,
+                            MINIMAL_DISTANCE,
+                            locationListener
+                        )
+                    }
+                }else{
+
+                    val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if(location==null) {
+                        //TODO почему не работает
+                        showDialog(getString(R.string.dialog_title_gps_turned_off),getString(R.string.dialog_message_last_location_unknown))
+                    }else{
+                        getAddress(context,location)
+                    }
+                }
+            }else{
+                showRatio()
+            }
+        }
+    }
+
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         with(binding) {
             with(viewModel) {
                 mainFragmentFAB.setOnClickListener { initListener() }
+                mainFragmentFABLocation.setOnClickListener { checkPermission() }
                 /*Инициализация viewModel, если не использовать lazy
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)*/
                 /*val observer = Observer<Any>{ Toast.makeText(context,"Работает ",Toast.LENGTH_LONG).show()}*/
